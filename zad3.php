c6eea7d894cdcb217048725d8c50e0145f62d067
Filip <filipz0203@gmail.com>
2019-04-17 16:41:40 +0200

    new

---
+++ zad3.php
@@ -0,0 +1,17 @@
+<?php
+//include($_SERVER['DOCUMENT_ROOT'] . "/lv2.1/LV2.xml");
+$inputFile = simplexml_load_file('LV2.xml');
+foreach($inputFile->record as $xmlPart) {
+    echo('<img src = "'.$xmlPart->slika.'"><br>');
+   
+    echo("Ime: " . "$xmlPart->ime <br>");
+	
+    echo("Prezime: " . "$xmlPart->prezime <br>");
+	
+    echo("e-mail: " . "$xmlPart->email <br>");
+	
+    echo("Spol: " . "$xmlPart->spol <br>");
+	
+    echo("Životopis: " . "$xmlPart->zivotopis <br><br>");
+}
+ ?>
